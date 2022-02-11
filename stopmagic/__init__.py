017c89d104653db24e7798eb66634280fb027ad0
Aldrin Mathew <admin@aldrinmathew.com>
2022-02-11 20:55:38 +0530

    `v0.3.0-beta` New version with major changes
    - Added onion skin feature. The colors and range are customisable. This
      feature is highly experimental and will also create extra meshes in
      your scene, so is not recommended for high poly meshes
    
    - Added ability to jump to the previous and next frame that has a keyed
      mesh present in it. If there is no such frame, nothing will happen
    
    - Added option to upgrade if there is a new version of the addon
      available. This is more dynamic and also makes use of network to make
      sure that the new version info is displayed correctly
    
    - Better Panel UI. Components are better displayed and spaced out and
      the UI is much more intuitive now

--- stopmagic/__init__.py
+++ stopmagic/__init__.py
@@ -24,10 +24,10 @@ __package__ = "stopmagic"
 bl_info = {
     "name": "Stopmagic",
     "author": "Aldrin Mathew",
-    "version": (0, 2, 1),
+    "version": (0, 3, 0),
     "blender": (2, 91, 0),
     "location": "Sidebar > Stopmagic",
-    "warning": "Experimental",
+    "warning": "beta",
     "category": "Animation",
     "description": "Improve your Stop motion workflow. Use shortcuts 'Ctrl Shift X' & 'Ctrl Shift Z' for faster workflows.",
     "doc_url": "https://github.com/aldrinsartfactory/stopmagic/wiki",
@@ -47,9 +47,15 @@ def register() -> None:
     operators.skip_frame_backward.register()
     operators.purge_unused_data.register()
     operators.initialize_handler.register()
+    operators.keyed_frame_next.register()
+    operators.keyed_frame_previous.register()
+    operators.upgrade_addon.register()
     bpy.app.handlers.load_post.append(functions.frame_handler)
+    bpy.app.handlers.load_post.append(functions.handle_onion_skin)
     bpy.app.handlers.frame_change_post.clear()
     bpy.app.handlers.frame_change_post.append(functions.update_stopmagic)
+    bpy.app.handlers.frame_change_pre.clear()
+    bpy.app.handlers.frame_change_pre.append(functions.handle_onion_skin)
     addon_keymaps = functions.key_config()
 
 
@@ -60,9 +66,14 @@ def unregister() -> None:
     operators.skip_frame_forward.unregister()
     operators.purge_unused_data.unregister()
     operators.initialize_handler.unregister()
+    operators.keyed_frame_next.unregister()
+    operators.keyed_frame_previous.unregister()
+    operators.upgrade_addon.unregister()
     panel.unregister()
-    bpy.app.handlers.load_post.remove(functions.frame_handler)
+    bpy.app.handlers.load_post.clear()
+    bpy.app.handlers.load_post.clear()
     bpy.app.handlers.frame_change_post.clear()
+    bpy.app.handlers.frame_change_pre.clear()
     addon_keymaps.clear()
 
 
