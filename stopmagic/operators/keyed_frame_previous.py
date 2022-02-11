23eca9c6b701d236e721c58a2f393756f37a0be5
Aldrin Mathew <admin@aldrinmathew.com>
2022-02-11 19:39:05 +0530

    Added `KeyedFramePrevious` Operator
    - Added `KeyedFramePrevious` operator which will search for all
      keyframes associated with the object and will jump to the previous
      frame with a keyed mesh if present.

---
+++ stopmagic/operators/keyed_frame_previous.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from typing import List, Set
+import bpy
+
+from stopmagic.functions.get_object_keyframes import get_object_keyframes
+
+
+class KeyedFramePrevious(bpy.types.Operator):
+    """Find and jump to the previous frame that has a mesh keyframe for the current object"""
+
+    bl_idname = "object.keyed_frame_previous"
+    bl_label = "Jump Previous"
+    bl_options = {"REGISTER", "UNDO"}
+
+    @classmethod
+    def poll(cls, context: bpy.types.Context) -> bool:
+        return context.view_layer.objects.active is not None
+
+    def execute(self, context: bpy.types.Context) -> Set[int] | Set[str]:
+        keyframes = get_object_keyframes()
+        if len(keyframes) > 0:
+            frame = bpy.context.scene.frame_current
+            keyframes = [k for k in keyframes if k < frame]
+        if len(keyframes) > 0:
+            highest = keyframes[0]
+            for num in keyframes:
+                if num > highest:
+                    highest = num
+            context.scene.frame_current = highest
+        return {"FINISHED"}
+
+
+def register() -> None:
+    bpy.utils.register_class(KeyedFramePrevious)
+
+
+def unregister() -> None:
+    bpy.utils.unregister_class(KeyedFramePrevious)
