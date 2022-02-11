810b6cf13bb6a7fc3666f412dbde09060fa33c76
Aldrin Mathew <admin@aldrinmathew.com>
2022-02-11 20:50:27 +0530

    Added more field in `StopmagicPreferences`
    - Added handler functions for in-scene value updates
    
    - Added more fields in StopmagicPreferences to support the new operators
      and functions
    
    - All properties used by the addon in the panel can now be synced with
      the preferences. Setting value in preferences will cause the next file
      you create in Blender to use those values

--- stopmagic/preferences.py
+++ stopmagic/preferences.py
@@ -1,6 +1,30 @@
 import bpy
 
-from stopmagic.functions import get_preferences
+from stopmagic.functions import (
+    get_preferences,
+    change_onion_color,
+    clear_onion_data,
+    handle_onion_skin,
+)
+
+
+def handle_onion_enable(self, value) -> None:
+    if bpy.context.scene.stopmagic_onion_skin_enabled:
+        handle_onion_skin(bpy.context.scene)
+    else:
+        clear_onion_data()
+
+
+def handle_past_color(self, value) -> None:
+    change_onion_color(True, bpy.context.scene.stopmagic_past_color)
+
+
+def handle_future_color(self, value) -> None:
+    change_onion_color(False, bpy.context.scene.stopmagic_future_color)
+
+
+def handle_offset(self, value) -> None:
+    handle_onion_skin(bpy.context.scene)
 
 
 class StopmagicPreferences(bpy.types.AddonPreferences):
@@ -26,14 +50,70 @@ class StopmagicPreferences(bpy.types.AddonPreferences):
         options=set(),
         default=True,
     )
+    onion_skin_enabled: bpy.props.BoolProperty(
+        name="Enable Onion Skin",
+        description="Whether onion skins should be displayed",
+        options=set(),
+        default=False,
+    )
+    past_color: bpy.props.FloatVectorProperty(
+        name="Color",
+        description="Color to be used for past frames of the Onion Skin",
+        subtype="COLOR_GAMMA",
+        size=4,
+        default=(1.0, 0.0, 0.0, 0.3),
+    )
+    future_color: bpy.props.FloatVectorProperty(
+        name="Color",
+        description="Color to be used for future frames of the Onion Skin",
+        subtype="COLOR_GAMMA",
+        size=4,
+        default=(0.0, 0.0, 1.0, 0.3),
+    )
+    past_offset: bpy.props.IntProperty(
+        name="Offset",
+        description="Show keyed meshes in these many frames before the current frame",
+        subtype="NONE",
+        options=set(),
+        default=10,
+        min=0,
+        max=200,
+        soft_min=0,
+        soft_max=100,
+        step=1,
+    )
+    future_offset: bpy.props.IntProperty(
+        name="Offset",
+        description="Show keyed meshes in these many frames after the current frame",
+        subtype="NONE",
+        options=set(),
+        default=10,
+        min=0,
+        max=200,
+        soft_min=0,
+        soft_max=100,
+        step=1,
+    )
 
-    def draw(self, context):
+    def draw(self, context: bpy.types.Context):
         self.layout.label(text="These settings will be used when you create a new file")
         self.layout.prop(self, "insert_keyframe_after_skip")
         row = self.layout.row()
         column = row.column()
         column.prop(self, "frame_skip_count")
-        column = row.column()
+        column.separator()
+        column = self.layout.column()
+        column.label(text="Onion Skin Options")
+        column.prop(self, "onion_skin_enabled")
+        row = column.row()
+        lcolumn = row.column()
+        lcolumn.label(text="Past")
+        lcolumn.prop(self, "past_offset")
+        lcolumn.prop(self, "past_color")
+        rcolumn = row.column()
+        rcolumn.label(text="Future")
+        rcolumn.prop(self, "future_offset")
+        rcolumn.prop(self, "future_color")
 
 
 def register_properties() -> None:
@@ -49,6 +129,55 @@ def register_properties() -> None:
         soft_max=100,
         step=1,
     )
+    bpy.types.Scene.stopmagic_past_offset = bpy.props.IntProperty(
+        name="Offset",
+        description="Show keyed meshes in these many frames before the current frame",
+        subtype="NONE",
+        options=set(),
+        default=get_preferences(__package__).past_offset,
+        min=0,
+        max=200,
+        soft_min=0,
+        soft_max=100,
+        step=1,
+        update=handle_offset,
+    )
+    bpy.types.Scene.stopmagic_future_offset = bpy.props.IntProperty(
+        name="Offset",
+        description="Show keyed meshes in these many frames after the current frame",
+        subtype="NONE",
+        options=set(),
+        default=get_preferences(__package__).future_offset,
+        min=0,
+        max=200,
+        soft_min=0,
+        soft_max=100,
+        step=1,
+        update=handle_offset,
+    )
+    bpy.types.Scene.stopmagic_past_color = bpy.props.FloatVectorProperty(
+        name="",
+        description="Color for past frames of the Onion Skin",
+        subtype="COLOR_GAMMA",
+        size=4,
+        default=get_preferences(__package__).past_color,
+        update=handle_past_color,
+    )
+    bpy.types.Scene.stopmagic_future_color = bpy.props.FloatVectorProperty(
+        name="",
+        description="Color for future frames of the Onion State",
+        subtype="COLOR_GAMMA",
+        size=4,
+        default=get_preferences(__package__).future_color,
+        update=handle_future_color,
+    )
+    bpy.types.Scene.stopmagic_onion_skin_enabled = bpy.props.BoolProperty(
+        name="Enable",
+        description="Whether Onion Skins should be displayed",
+        options=set(),
+        default=get_preferences(__package__).onion_skin_enabled,
+        update=handle_onion_enable,
+    )
     bpy.types.Scene.stopmagic_insert_frame_after_skip = bpy.props.BoolProperty(
         name="Insert Keyframe",
         description="Whether to insert keyframe after skipping frames",
@@ -60,6 +189,11 @@ def register_properties() -> None:
 def unregister_properties() -> None:
     del bpy.types.Scene.stopmagic_frame_skip_count
     del bpy.types.Scene.stopmagic_insert_frame_after_skip
+    del bpy.types.Scene.stopmagic_past_offset
+    del bpy.types.Scene.stopmagic_future_offset
+    del bpy.types.Scene.stopmagic_past_color
+    del bpy.types.Scene.stopmagic_future_color
+    del bpy.types.Scene.stopmagic_onion_skin_enabled
 
 
 def register() -> None:
@@ -70,3 +204,7 @@ def register() -> None:
 def unregister() -> None:
     bpy.utils.unregister_class(StopmagicPreferences)
     unregister_properties()
+
+
+if __name__ == "__main__":
+    register()
