2bc0b9cde05529238de81d5384d2b2d1cc779293
Aldrin Mathew <admin@aldrinmathew.com>
2022-02-16 23:29:39 +0530

    Updated Addon Preferences
    - Added option to choose the type of display for Onion Skin
    
    - Added field to specify the number of poses for the past and future
      variants
    
    - Added values so as to programmatically expand parts of the panel UI

--- stopmagic/preferences.py
+++ stopmagic/preferences.py
@@ -23,7 +23,7 @@ def handle_future_color(self, value) -> None:
     change_onion_color(False, bpy.context.scene.stopmagic_future_color)
 
 
-def handle_offset(self, value) -> None:
+def handle_display(self, value) -> None:
     handle_onion_skin(bpy.context.scene)
 
 
@@ -70,9 +70,31 @@ class StopmagicPreferences(bpy.types.AddonPreferences):
         size=4,
         default=(0.0, 0.0, 1.0, 0.3),
     )
+    onion_display_type: bpy.props.EnumProperty(
+        items=[
+            (
+                "POSE",
+                "Pose",
+                "Display a specific number of poses before or after the current frame",
+                "CON_ACTION",
+                0,
+            ),
+            (
+                "RANGE",
+                "Range",
+                "Display all poses within a range before or after the current frame",
+                "ARROW_LEFTRIGHT",
+                1,
+            ),
+        ],
+        name="Display Type",
+        description="Change the type of display for the onion skin",
+        options=set(),
+        default="POSE",
+    )
     past_offset: bpy.props.IntProperty(
         name="Offset",
-        description="Show keyed meshes in these many frames before the current frame",
+        description="Show onion skins for these many frames before the current frame",
         subtype="NONE",
         options=set(),
         default=10,
@@ -82,9 +104,21 @@ class StopmagicPreferences(bpy.types.AddonPreferences):
         soft_max=100,
         step=1,
     )
+    past_count: bpy.props.IntProperty(
+        name="Past Count",
+        description="Show onion skins for this many mesh poses, before the current frame",
+        subtype="NONE",
+        options=set(),
+        default=1,
+        min=1,
+        max=100,
+        soft_min=1,
+        soft_max=10,
+        step=1,
+    )
     future_offset: bpy.props.IntProperty(
         name="Offset",
-        description="Show keyed meshes in these many frames after the current frame",
+        description="Show onion skins for these many frames after the current frame",
         subtype="NONE",
         options=set(),
         default=10,
@@ -92,6 +126,18 @@ class StopmagicPreferences(bpy.types.AddonPreferences):
         max=200,
         soft_min=0,
         soft_max=100,
+        step=1,
+    )
+    future_count: bpy.props.IntProperty(
+        name="Future Count",
+        description="Show onion skins for this many mesh poses, after the current frame",
+        subtype="NONE",
+        options=set(),
+        default=1,
+        min=1,
+        max=100,
+        soft_min=1,
+        soft_max=10,
         step=1,
     )
 
@@ -105,13 +151,16 @@ class StopmagicPreferences(bpy.types.AddonPreferences):
         column = self.layout.column()
         column.label(text="Onion Skin Options")
         column.prop(self, "onion_skin_enabled")
+        column.prop(self, "onion_display_type")
         row = column.row()
         lcolumn = row.column()
         lcolumn.label(text="Past")
+        lcolumn.prop(self, "past_count")
         lcolumn.prop(self, "past_offset")
         lcolumn.prop(self, "past_color")
         rcolumn = row.column()
         rcolumn.label(text="Future")
+        rcolumn.prop(self, "future_count")
         rcolumn.prop(self, "future_offset")
         rcolumn.prop(self, "future_color")
 
@@ -129,6 +178,42 @@ def register_properties() -> None:
         soft_max=100,
         step=1,
     )
+    bpy.types.Scene.stopmagic_onion_display_type = bpy.props.EnumProperty(
+        items=[
+            (
+                "POSE",
+                "Pose",
+                "Display a specific number of poses before or after the current frame",
+                "CON_ACTION",
+                0,
+            ),
+            (
+                "RANGE",
+                "Range",
+                "Display all poses within a range before or after the current frame",
+                "ARROW_LEFTRIGHT",
+                1,
+            ),
+        ],
+        name="Display",
+        description="Change the type of display for the onion skin",
+        options=set(),
+        default=get_preferences(__package__).onion_display_type,
+        update=handle_display
+    )
+    bpy.types.Scene.stopmagic_past_count = bpy.props.IntProperty(
+        name="Poses",
+        description="Show onion skins for this many mesh poses, before the current frame",
+        subtype="NONE",
+        options=set(),
+        default=get_preferences(__package__).past_count,
+        min=1,
+        max=100,
+        soft_min=1,
+        soft_max=10,
+        step=1,
+        update=handle_display
+    )
     bpy.types.Scene.stopmagic_past_offset = bpy.props.IntProperty(
         name="Offset",
         description="Show keyed meshes in these many frames before the current frame",
@@ -140,7 +225,20 @@ def register_properties() -> None:
         soft_min=0,
         soft_max=100,
         step=1,
-        update=handle_offset,
+        update=handle_display,
+    )
+    bpy.types.Scene.stopmagic_future_count = bpy.props.IntProperty(
+        name="Poses",
+        description="Show onion skins for this many mesh poses, after the current frame",
+        subtype="NONE",
+        options=set(),
+        default=get_preferences(__package__).future_count,
+        min=1,
+        max=100,
+        soft_min=1,
+        soft_max=10,
+        step=1,
+        update=handle_display
     )
     bpy.types.Scene.stopmagic_future_offset = bpy.props.IntProperty(
         name="Offset",
@@ -153,7 +251,7 @@ def register_properties() -> None:
         soft_min=0,
         soft_max=100,
         step=1,
-        update=handle_offset,
+        update=handle_display,
     )
     bpy.types.Scene.stopmagic_past_color = bpy.props.FloatVectorProperty(
         name="",
@@ -172,7 +270,7 @@ def register_properties() -> None:
         update=handle_future_color,
     )
     bpy.types.Scene.stopmagic_onion_skin_enabled = bpy.props.BoolProperty(
-        name="Enable",
+        name="Enable (Experimental)",
         description="Whether Onion Skins should be displayed",
         options=set(),
         default=get_preferences(__package__).onion_skin_enabled,
@@ -184,16 +282,46 @@ def register_properties() -> None:
         options=set(),
         default=get_preferences(__package__).insert_keyframe_after_skip,
     )
+    bpy.types.Scene.stopmagic_expand_find_frame = bpy.props.BoolProperty(
+        name="",
+        description="",
+        options=set(),
+        default=True,
+    )
+    bpy.types.Scene.stopmagic_expand_frame_skip = bpy.props.BoolProperty(
+        name="",
+        description="",
+        options=set(),
+        default=True,
+    )
+    bpy.types.Scene.stopmagic_expand_onion_skin = bpy.props.BoolProperty(
+        name="",
+        description="",
+        options=set(),
+        default=False,
+    )
+    bpy.types.Scene.stopmagic_expand_status_options = bpy.props.BoolProperty(
+        name="",
+        description="",
+        options=set(),
+        default=False,
+    )
 
 
 def unregister_properties() -> None:
     del bpy.types.Scene.stopmagic_frame_skip_count
     del bpy.types.Scene.stopmagic_insert_frame_after_skip
     del bpy.types.Scene.stopmagic_past_offset
+    del bpy.types.Scene.stopmagic_past_count
     del bpy.types.Scene.stopmagic_future_offset
+    del bpy.types.Scene.stopmagic_future_count
     del bpy.types.Scene.stopmagic_past_color
     del bpy.types.Scene.stopmagic_future_color
     del bpy.types.Scene.stopmagic_onion_skin_enabled
+    del bpy.types.Scene.stopmagic_expand_find_frame
+    del bpy.types.Scene.stopmagic_expand_frame_skip
+    del bpy.types.Scene.stopmagic_expand_onion_skin
+    del bpy.types.Scene.stopmagic_expand_status_options
 
 
 def register() -> None:
