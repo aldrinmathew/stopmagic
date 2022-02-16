2eeac5feebf9b65c347c09cb8df1cea697d55e30
Aldrin Mathew <admin@aldrinmathew.com>
2022-02-16 22:55:00 +0530

    Added `get_object_key_values`
    - Added function `get_object_key_values` that gets all the keyed values
      of the meshes
    
    - The key value is an integer that is the corresponding index for the
      active mesh for the object.

--- stopmagic/functions/get_object_key_values.py
+++ stopmagic/functions/get_object_key_values.py
@@ -1,12 +1,9 @@
-from typing import List
 import bpy
+from typing import List
 
 
-def get_object_keyframes(obj: bpy.types.Object) -> List[int]:
-    """Get all keyframes that has mesh keyframes, associated with the active object"""
-    keyframes: List[int] = []
-    if obj is None:
-        obj = bpy.context.view_layer.objects.active
+def get_object_key_values(obj: bpy.types.Object) -> List[int]:
+    values: List[int] = []
     if obj.get("sm_id") is not None:
         for action in bpy.data.actions:
             if obj.user_of_id(action.id_data):
@@ -19,9 +16,9 @@ def get_object_keyframes(obj: bpy.types.Object) -> List[int]:
                         #
                         keyframe_points = fcurve.keyframe_points
                         for item in keyframe_points:
-                            i = 0
+                            i = 1
                             while i < len(item.co):
-                                keyframes.append(int(item.co[i]))
+                                values.append(int(item.co[i]))
                                 i += 2
-                        return keyframes
-    return keyframes
+                        return values
+    return values
